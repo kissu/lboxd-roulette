9bef46de98179b49a81ca7c11cfbc4493bc9c749
maia <5vitorferreira2003@gmail.com>
2022-04-29 17:43:08 -0300

    flip flip

--- app.vue
+++ app.vue
@@ -6,8 +6,8 @@ useHead({
 </script>
 
 <script>
-//import './assets/style.css'
 import csv from './assets/filmList.csv'
+import './assets/css/card.css'
 
 export default { data() { return {
   films: [],
@@ -27,14 +27,36 @@ methods: {
     }
   },
 
+  resetFilms() {
+    for (let i=0; i<10 ; i++) {
+      this.films[i]['hidden'] = true
+    }
+    setTimeout(() => {this.rollFilms()}, 500);
+  },
+
   rollFilms() {
     this.films = []
     this.prev = false
-    for (let i=0; i<10 ; i++) {
-      let selecFilm = csv[Math.floor(Math.random()*1000)]
-      this.films.push({'hidden':true,'off':false,'href':selecFilm.href,'src':'https://a.ltrbxd.com/resized/'+selecFilm.src})
+
+    const nums = new Set();
+    while (nums.size !== 10) {
+      nums.add(Math.floor(Math.random() * 1400) + 1);
+    }
+
+    for (let item of nums) {
+      let selecFilm = csv[item]
+      this.films.push({'hidden':true,'off':false,'select':false,href:selecFilm.href,'src':'https://a.ltrbxd.com/resized/'+selecFilm.src})
     }
   },
+
+  selectFilm() {
+    this.films[this.prev]['select'] = true
+    for (let i=0; i<10 ; i++) {
+      if (i == this.prev) { continue }
+      this.films[i]['hidden'] = false
+      this.films[i]['off'] = true
+    }
+  }
   
   },
   beforeMount(){
@@ -44,41 +66,38 @@ methods: {
 </script>
 
 <template>
-<body class="font-graph m-0 h-full">
-  <header class="h-16 bg-[#14181c] block px-32">
+<body class="m-0 h-full font-graph">
+  <header class="h-16 bg-[#14181c] block px-[15%]">
     <div class="flex items-center float-left">
       <img class="h-16" src="./assets/logo.svg">
       <h1 class="font-bold text-4xl pb-[7px]">Roulette</h1>
     </div>
-    <div class="float-right font-semig h-full flex">
-      <button class="but bg-blue-500 my-auto" @click="rollFilms()">RANDOM</button>
+    <div class="float-right h-full flex gap-5">
     </div>
     
   </header>
-  <div class="app flex-1 max-w-5xl mx-auto p-12" >
-    <div class="films mx-auto grid grid-cols-5 gap-6">
-        <div v-for="(film,i) in films"
-          class="image hover:border-solid overflow-hidden relative"
-          :class="{ 'grayscale':film.off, 'brightness-0':film.hidden }"
-          @click="showFilm(i)"
-        >
-          <img class="w-full h-full" :src="film.src">
+  <div class="max-w-5xl mx-auto flex flex-col align-center">
+    <div class="mx-auto grid grid-cols-5 gap-7 p-12">
+      <div v-for="(film,i) in films"
+        class="card hover:scale-105 transition-transform"
+        @click="showFilm(i)">
+        <div class="inner" :class="{ 'flip':!film.hidden }">
+          <div class="back"></div>
+          <img class="front" :class="{ 'grayscale':film.off, 'selected':film.select }" :src="film.src">    
         </div>
+      </div>
+    </div>
+    <div class="flex mx-auto gap-6">
+      <button class="but bg-lime-600 my-auto" @click="selectFilm()">LOCK</button>
+      <button class="but bg-blue-500 my-auto" @click="resetFilms()">RESET</button>
     </div>
   </div>
-
-  <!-- <div class="modal-container">
+  
+  <div class="modal-container">
     <div class="modal">
-      <div>
-        <h2>Letterboxd Roulette</h2>
-        <svg viewbox="0 0 24 24" stroke-width="3" stroke-linecap="round" stroke="#fff">
-          <line x1="18" y1="6" x2="6" y2="18"/>
-          <line x1="6" y1="6" x2="18" y2="18"/>
-        </svg>
-      </div>  
-      
-      <p>Inspired by <a  class="font-bold" href="https://letterboxd.com/tobiasandersen2/list/random-movie-roulette/">this list</a></p>
+      <h2>helppp</h2>
+      <p>pogggers</p>
     </div>
-  </div> -->
+  </div>
 </body>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,9 @@
 module.exports = {
-  content: [
-    "./*.vue",
-    "./nuxt.config.{js,ts}",
-  ],
+  content: ["./*.vue", "./nuxt.config.{js,ts}"],
   theme: {
     extend: {
       fontFamily: {
-        graph: "'Graphik-Regular-Web': sans-serif",
-        semig: "'Graphik-Semibold-Web': sans-serif"
+        'graph':['Work Sans']
       },
     },
   },
